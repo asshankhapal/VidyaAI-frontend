d45e49bc95f8320950bc09b51a1e934a06f29cfb
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-09-07 22:59:17 +0530

    React and TailwindCSS plugins proper

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import AppRouter from "./AppRouter";
 
 function App() {
   return (
-    <>
+    <> 
+      <div className="app-header">
+        <h1>Tailwind CSS is added </h1>
+      </div>
       <AppRouter />
     </>
   );
