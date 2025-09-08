b6071678f8ac396137646639244027f08c648d7a
bhaveshka23 <bhaveshkale419@gmail.com>
2025-09-08 10:56:14 +0530

    LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import AppRouter from "./AppRouter";
 
 function App() {
   return (
-    <> 
-      <div className="app-header">
-        <h1>Tailwind CSS is added </h1>
-      </div>
-      <AppRouter />
-    </>
+    <AppRouter />
   );
 }
 
--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
